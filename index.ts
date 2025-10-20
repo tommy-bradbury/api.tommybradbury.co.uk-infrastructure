82a09cd391d900153b247b9c7f3869738d44e339
Tommy <tommy.j.bradbury@gmail.com>
2025-10-20 22:24:04 +0100

    sort fpm layer for php request handlinn

--- index.ts
+++ index.ts
@@ -2,13 +2,11 @@ import * as aws from "@pulumi/aws";
 import * as pulumi from "@pulumi/pulumi";
 import * as path from "path";
 
-// --- Configuration ---
 const accountId = aws.getCallerIdentity().then(identity => identity.accountId);
 const domainName = "api.tommybradbury.co.uk";
 const certificateArn = "arn:aws:acm:eu-west-2:140293477718:certificate/dc46af13-b4c4-4759-bf96-5b5b04444b4f";
-const brefPhpLayerArn = "arn:aws:lambda:eu-west-2:534081306603:layer:php-84:34";
+const brefPhpLayerArn = "arn:aws:lambda:us-east-1:534081306603:layer:php-84-fpm:34";
 
-// --- IAM Role for Lambda ---
 const role = new aws.iam.Role("lambdaRole", {
     assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({ Service: "lambda.amazonaws.com" }),
 });
@@ -18,13 +16,18 @@ new aws.iam.RolePolicyAttachment("lambdaPolicyAttachment", {
     policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole,
 });
 
-// --- API Gateway ---
 const api = new aws.apigatewayv2.Api("httpApi", {
     protocolType: "HTTP",
     name: "api.tommybradbury.co.uk-auth-service",
 });
 
-// --- Helper Function for Lambda Creation ---
+/**
+ * create bref php lambda function
+ * @param name 
+ * @param zipFileName 
+ * @param description 
+ * @returns 
+ */
 function createBrefFunction(name: string, zipFileName: string, description: string) {
     const lambdaFunction = new aws.lambda.Function(name, {
         name: name,
@@ -56,14 +59,12 @@ function createBrefFunction(name: string, zipFileName: string, description: stri
     return { function: lambdaFunction, version: lambdaFunction.version, alias: devAlias, permission: permission };
 }
 
-// --- Lambda Function ---
 const authService = createBrefFunction(
     "AuthServiceLambda",
     "lambda-auth.zip",
     "Handles /auth/ route"
 );
 
-// --- API Gateway Integration ---
 const authIntegration = new aws.apigatewayv2.Integration("authIntegration", {
     apiId: api.id,
     integrationType: "AWS_PROXY",
@@ -89,14 +90,31 @@ const deployment = new aws.apigatewayv2.Deployment("apiDeployment", {
     apiId: api.id,
 }, { dependsOn: [authANYRoute, authANYProxyRoute] });
 
+const httpLogs = new aws.cloudwatch.LogGroup("httpApiAccessLogs", { retentionInDays: 14 });
+
 const stage = new aws.apigatewayv2.Stage("apiStage", {
-    apiId: api.id,
-    name: "$default",
-    autoDeploy: true,
+  apiId: api.id,
+  name: "$default",
+  autoDeploy: true,
+  accessLogSettings: {
+    destinationArn: httpLogs.arn,
+    format: JSON.stringify({
+      requestId: "$context.requestId",
+      time: "$context.time",
+      httpMethod: "$context.httpMethod",
+      path: "$context.path",
+      routeKey: "$context.routeKey",
+      status: "$context.status",
+      integrationStatus: "$context.integrationStatus",
+      errorMessage: "$context.error.message",
+      integrationErrorMessage: "$context.integrationErrorMessage",
+      responseLength: "$context.responseLength",
+      ip: "$context.identity.sourceIp",
+      userAgent: "$context.identity.userAgent",
+    }),
+  },
 });
 
-// --- Custom Domain ---
-// FIX: Changed from .get() to new aws.apigatewayv2.DomainName() to create the domain.
 const apiDomain = new aws.apigatewayv2.DomainName("apiDomain", {
     domainName: domainName,
     domainNameConfiguration: {
@@ -106,7 +124,6 @@ const apiDomain = new aws.apigatewayv2.DomainName("apiDomain", {
     },
 });
 
-// --- API Mapping ---
 const apiMapping = new aws.apigatewayv2.ApiMapping("apiMapping", {
     apiId: api.id,
     domainName: apiDomain.domainName,
@@ -117,7 +134,6 @@ const apiMapping = new aws.apigatewayv2.ApiMapping("apiMapping", {
 export const apiUrl = api.apiEndpoint;
 export const customApiUrl = `https://${domainName}/auth`;
 export const lambdaVersion = authService.version;
-// FIX: Use the newly created domain resource for the outputs.
 export const dnsTargetDomainName = apiDomain.domainNameConfiguration.apply(dnc => dnc.targetDomainName);
 export const dnsTargetHostedZoneId = apiDomain.domainNameConfiguration.apply(dnc => dnc.hostedZoneId);
 
